cb40b283a01716f87c93b5bbec4498b8c4338f06
Rithin Lehan <74766580+Rithingithub@users.noreply.github.com>
2022-04-16 20:09:07 +0530

    Update tweepy.py

--- tweepy.py
+++ tweepy.py
@@ -2,10 +2,10 @@ import tweepy
 
 
 # API keyws that yous saved earlier
-api_key = "RSZjBFIKELSmycKKB9rvXLtp9"
-api_secrets = "IF1haNyRkHsaQb5tFtIcT2kpojmUfgYlslhLdys7J3m9qRvUQG"
-access_token = "1515311399878422542-9IuvjqLtrc7rqGgxgrHg3HTHT7LmrC"
-access_secret = "1N52oC1djy7Atrvf79KG2WRwDzbCD5sqkYfT8yiuUjtMV"
+api_key = "<API_KEY>"
+api_secrets = "<API_SECRET>"
+access_token = "<ACCESS_TOKEN>"
+access_secret = "<ACCESS_SECRET>"
 
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler(api_key, api_secrets)
