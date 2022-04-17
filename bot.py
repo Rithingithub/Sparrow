50fae31e279dbe2f50fea1ef5e515911ad4ce398
FrnMhd <96841164+FrnMhd@users.noreply.github.com>
2022-04-17 10:33:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,56 +1,50 @@
+#SparrowTeambot -Twitter Bot
+
 import requests
 import keys
 import pandas as pd
 import tweepy
-import os
-
-
+import time
 
 def get_crypto_rates(base_currency='INR', assets='BTC,ETH,XRP'):
-    url = 'https://api.nomics.com/v1/currencies/ticker'
-    payload = {'key': keys.API_KEY1, 'convert': base_currency, 'ids': assets, 'interval': '1d'}
-    response = requests.get(url, params=payload)
-    data = response.json()
+        url = 'https://api.nomics.com/v1/currencies/ticker'
+        payload = {'key': keys.API_KEY1, 'convert': base_currency, 'ids': assets, 'interval': '1d'}
+        response = requests.get(url, params=payload)
+        data = response.json()
+        crypto_currency, crypto_price, crypto_timestamp = [], [], []
 
-    crypto_currency, crypto_price, crypto_timestamp = [], [], []
+        #print(data)
+        for asset in data:
+            crypto_currency.append(asset['currency'])
+            crypto_price.append(asset['price'])
+            crypto_timestamp.append(asset['price_timestamp'])
 
-    print(data)
-    for asset in data:
-        crypto_currency.append(asset['currency'])
-        crypto_price.append(asset['price'])
-        crypto_timestamp.append(asset['price_timestamp'])
+        raw_data = {
+            'Assets': crypto_currency,
+            'Rate': crypto_price,
+            'Timestamp': crypto_timestamp
+        }
 
-    raw_data = {
-        'assets': crypto_currency,
-        'Rate': crypto_price,
-        'timestamp': crypto_timestamp
-    }
+        df = pd.DataFrame(raw_data)
+        print(df)
+        return df.to_string()
 
-    df = pd.DataFrame(raw_data)
-    print(df)
-    return df
-
-
-get_crypto_rates()
+while(True):
 
 
-API_KEY = '<API_KEY>'
-API_SECRET = '<API_SECRET>'
-#API_TOKEN = '<API_TOKEN>'
-ACCESS_TOKEN = '<ACCESS_KEY>'
-ACCESS_SECRET = '<ACCESS_SECRET>'
 
 
-auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+    res=get_crypto_rates()
 
-api = tweepy.API(auth)
 
-#tweet_list = api.favorites(screen_name='rithin_lehan', count=5)
-#print(tweet_list)
-
-api.update_status(status="Welcome Twitter ")
-
+    client = tweepy.Client(bearer_token=keys.bearer_token,
+                           consumer_key=keys.consumer_key,
+                           consumer_secret=keys.consumer_secret,
+                           access_token=keys.access_token,
+                           access_token_secret=keys.access_token_secret)
+    client.create_tweet(text=res,user_auth=True)
+    print("Waiting 60 Minute...")
+    time.sleep(3600) #runs hourly
 
 #Retweet
 for tweet in api.favorite(screen_name='Bitcoin'):
