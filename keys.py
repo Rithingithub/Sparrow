1f1a9e617ce751ccf1deaf5eeacfcf97fab7f586
FrnMhd <96841164+FrnMhd@users.noreply.github.com>
2022-04-17 10:49:08 +0530

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,1 +1,6 @@
-
+API_KEY1 = 'APIKEY' #nomics api key
+bearer_token="BEARER_TOKEN" #twitter bearer token
+consumer_key='CONSUMERKEY'
+consumer_secret='CONSUMER_SECRET'
+access_token='ACCESS_TOKEN'
+access_token_secret='ACCESS_SECRET'
